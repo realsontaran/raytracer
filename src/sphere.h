462b7fe2175b200a21e325425070ec4a28df9d12
penguin <e.ozbay75@gmail.com>
2022-04-29 12:41:53 +0300

    Hitting spheres

--- src/sphere.h
+++ src/sphere.h
@@ -1,19 +1,48 @@
-#ifndef SPHERE_H_
-#define SPHERE_H_
+#ifndef SPHERE_H
+#define SPHERE_H
 
 #include "hittable.h"
 #include "vector3d.h"
-
-class Sphere : Hittable {
+#include <cmath>
+class sphere : public Hittable {
 public:
-  Sphere() {}
-  Sphere(point3d c, double r) : center(c), radius(r){};
-  virtual bool hit(const Ray &r, double t_min, double t_max,
-                   hit_data &data) const override;
+  sphere() {}
+  sphere(point3d cen, double r) : center(cen), radius(r){};
 
-private:
+  virtual bool hit(const Ray &r, double t_min, double t_max,
+                   hit_data &rec) const override;
+
+public:
   point3d center;
   double radius;
 };
 
-#endif // SPHERE_H_
+inline bool sphere::hit(const Ray &r, double t_min, double t_max,
+                        hit_data &rec) const {
+  vector3d oc = r.start() - center;
+  auto a = pow(r.direction().length(), 2);
+  auto half_b = dot(oc, r.direction());
+  auto c = pow(oc.length(), 2) - radius * radius;
+
+  auto discriminant = half_b * half_b - a * c;
+  if (discriminant < 0)
+    return false;
+  auto sqrtd = sqrt(discriminant);
+
+  // Find the nearest root that lies in the acceptable range.
+  auto root = (-half_b - sqrtd) / a;
+  if (root < t_min || t_max < root) {
+    root = (-half_b + sqrtd) / a;
+    if (root < t_min || t_max < root)
+      return false;
+  }
+
+  rec.t = root;
+  rec.p = r.at(rec.t);
+  vector3d outward_normal = (rec.p - center) / radius;
+  rec.set_face_normal(r, outward_normal);
+
+  return true;
+}
+
+#endif
