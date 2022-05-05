5d73be4f028f39e5b27f452c8d6b795b4f0587ef
penguin <e.ozbay75@gmail.com>
2022-05-05 23:34:22 +0300

    Threads

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,17 @@
 #include "ray.h"
 #include "triangle.h"
 #include "vector3d.h"
-#include <algorithm>
 #include <cmath>
+#include <condition_variable>
 #include <fstream>
+#include <future>
 #include <iostream>
+#include <iterator>
 #include <limits>
 #include <memory>
+#include <mutex>
+#include <thread>
+#include <vector>
 
 using std::fabs;
 using std::pow;
@@ -24,23 +29,22 @@ color ray_color(const Ray &r, Scene scene, int depth) {
     for (const auto light : scene.point_lights) {
       Ray light_ray = Ray(rec.p, light.position - rec.p);
       vector3d wi = (light.position - rec.p);
-      color ambient =
-          clamp(rec.material->ambient * scene.ambient_light, 0, 255);
 
       hit_data tmp;
       if (scene.hit(light_ray, 0.0001, std::numeric_limits<double>::infinity(),
                     tmp))
         continue;
-
+      // Ambient
+      color ambient =
+          clamp(rec.material->ambient * scene.ambient_light, 0, 255);
+      // Diffuse
       const auto irradiance = light.intensity / wi.length_square();
-
       const auto diffuse_coeff = rec.material->diffuse;
-
       double cosTheta = dot(unit_vector(wi), unit_vector(rec.n));
       cosTheta = (cosTheta < 0.0) ? 0.0 : cosTheta;
-
       auto diffuse = cosTheta * irradiance * diffuse_coeff;
 
+      // Specular
       auto h = wi + (scene.camera.e - rec.p);
       h /= vector3d(fabs(h.x()), fabs(h.y()), fabs(h.z()));
       auto cosalpha = dot(unit_vector(rec.n), unit_vector(h));
@@ -48,12 +52,15 @@ color ray_color(const Ray &r, Scene scene, int depth) {
       auto exp = pow(cosalpha, rec.material->phong);
       auto specular = exp * rec.material->specular * irradiance;
 
+      // Mirror
+      auto wo = unit_vector(rec.p - scene.camera.e);
+      auto wr = -wo + 2 * dot(unit_vector(rec.n), wo) * unit_vector(rec.n);
+
       auto result = diffuse;
       result += ambient;
       result += specular;
 
       const auto target = rec.p + rec.n;
-
       result = result + ray_color(Ray(rec.p, target - rec.p), scene, depth - 1);
       return clamp(result, 0, 255);
     }
@@ -61,22 +68,62 @@ color ray_color(const Ray &r, Scene scene, int depth) {
   return scene.bg_color;
 }
 
+std::vector<color> threadColor(int start, int end, Scene scene, int depth) {
+  std::vector<color> result;
+  const int image_width = scene.camera.plane.nx;
+
+  for (int j = start; j < end; ++j) {
+    Ray r = scene.camera.getRay(j % image_width, j / image_width);
+    result.push_back(ray_color(r, scene, depth));
+  }
+  return result;
+}
+
 int main() {
   Parser *parser = new Parser(std::string("scene.xml"));
   parser->setScene();
   Scene scene = parser->getScene();
-  Camera camera = parser->getScene().camera;
   std::ofstream fout("output.ppm");
+  const int image_width = scene.camera.plane.nx;
+  const int image_height = scene.camera.plane.ny;
 
-  const int image_width = camera.plane.nx;
-  const int image_height = camera.plane.ny;
+  const int hw_t = std::thread::hardware_concurrency();
+  const int pixel_count = image_height * image_width;
+  const int pixel_per_thread = pixel_count / hw_t;
+  const int left_over = pixel_count % hw_t;
+
+  std::mutex mutex;
+  std::condition_variable cvResults;
+  std::vector<std::future<std::vector<color>>> m_futures;
   fout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-  for (int j = 0; j < image_height; ++j) {
-    for (int i = 0; i < image_width; ++i) {
-      Ray r = camera.getRay(i, j);
-      color pixel_color = ray_color(r, scene, scene.max_ray);
-      color_ouput(fout, pixel_color);
+  int start = 0;
+  int end = pixel_per_thread;
+  for (int j = 0; j < hw_t + 1; ++j) {
+    auto future = std::async(std::launch::async | std::launch::deferred,
+                             threadColor, start, end, scene, scene.max_ray);
+
+    start += pixel_per_thread;
+    end += pixel_per_thread;
+    if (j == hw_t) {
+      auto future =
+          std::async(std::launch::async | std::launch::deferred, threadColor,
+                     start, pixel_count, scene, scene.max_ray);
+    }
+    {
+      std::lock_guard<std::mutex> lock(mutex);
+      m_futures.push_back(std::move(future));
+    }
+  }
+
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    cvResults.wait(
+        lock, [&m_futures, &hw_t] { return m_futures.size() == hw_t + 1; });
+  }
+  for (std::future<std::vector<color>> &r : m_futures) {
+    for (color color : r.get()) {
+      color_ouput(fout, color);
     }
   }
 }
